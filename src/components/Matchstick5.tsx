729708bf1f3db152689e2a900f45d5663b2be706
ramiro lugo <ramiro.lugo@mi.unc.edu.ar>
2024-02-24 19:36:25 -0300

    remove image/next

--- src/components/Matchstick5.tsx
+++ src/components/Matchstick5.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { point } from "@/type";
 
 export default function Matchstick5({ points }: { points: point }) {
@@ -11,47 +10,37 @@ export default function Matchstick5({ points }: { points: point }) {
   };
   return (
     <div className="w-[120px] h-[120px]">
-      <Image
+      <img
         src="/assets/matchstick.svg"
         alt="fósforo de puntos"
-        width={120}
-        height={15}
         className={`${!match[1] ? "opacity-0" : "opacity-100"}
-           ml-[5%] h-auto w-[8rem] transition-all ease-in-out duration-100
+           ml-[5%] h-auto w-[8rem] transition-all ease-in-out duration-100 
           `}
       />
-      <Image
+      <img
         src="/assets/matchstick.svg"
         alt="fósforo de puntos"
-        width={120}
-        height={15}
         className={`${!match[2] ? "opacity-0" : "opacity-100"}
           rotate-90 ml-[50%] mt-[35%] h-auto w-[8rem] transition-all ease-in-out duration-100 
           `}
       />
-      <Image
+      <img
         src="/assets/matchstick.svg"
         alt="fósforo de puntos"
-        width={120}
-        height={15}
         className={`${!match[3] ? "opacity-0" : "opacity-100"}
            -rotate-90 -ml-[50%] -mt-[12%] h-auto w-[8rem] transition-all ease-in-out duration-100
           `}
       />
-      <Image
+      <img
         src="/assets/matchstick.svg"
         alt="fósforo de puntos"
-        width={120}
-        height={15}
         className={`${!match[4] ? "opacity-0" : "opacity-100"}
           rotate-180 mt-[35%] -ml-[5%] h-auto w-[8rem] transition-all ease-in-out duration-100
           `}
       />
-      <Image
+      <img
         src="/assets/matchstick.svg"
         alt="fósforo de puntos"
-        width={120}
-        height={15}
         className={`${!match[5] ? "opacity-0" : "opacity-100"}
           rotate-[40deg] -mt-[58%] h-auto w-[8rem] transition-all ease-in-out duration-100
           `}
